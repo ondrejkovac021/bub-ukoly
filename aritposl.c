62a0ffbb58c218d3d3e0afc72737cb0d36d9ceb1
Ondřej Kováč <96345636+ondrejkovac021@users.noreply.github.com>
2022-03-20 14:18:33 +0100

    Create aritposl.c

---
+++ aritposl.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int a1 = 14;
+    int d[7] = {0}; 
+    int n = 13;
+    for(int i = 0; i < n; i++)
+    {
+        d[n] += a1;
+    printf("%d|", d[n]);
+    }
+    return 0;
+}
